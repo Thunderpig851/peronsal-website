b79f67428f50a7f7d30e13b3911214d5e34f85fd
Thunderpig851 <87243196+Thunderpig851@users.noreply.github.com>
2025-09-30 17:38:55 -0400

    prod changes

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,21 +1,12 @@
-// svelte.config.js
-import adapter from '@sveltejs/adapter-auto';
+// svelte.config.ts
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
 const mdsvexConfig = { extensions: ['.md', '.svx'] };
-const dev = process.argv.includes('dev');
 
-const config = {
+export default {
   extensions: ['.svelte', ...mdsvexConfig.extensions],
   preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
-  kit: {
-    adapter: adapter(),
-    prerender: { entries: ['*'] },
-    paths: {
-      base: dev ? '' : '/my-site' // <-- change to your repo name
-    }
-  }
+  kit: { adapter: adapter() }
 };
-
-export default config;
